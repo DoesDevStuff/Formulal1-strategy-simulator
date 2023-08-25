09adbc65c476bc0ef8339fd617bb3c88587e591f
CharlieTheIndieDev <74312830+DoesDevStuff@users.noreply.github.com>
2023-08-25 16:01:32 +0100

    okay comments added and code refactored

--- src/race_logic/Car.java
+++ src/race_logic/Car.java
@@ -1,87 +1,88 @@
 package race_logic;
 
 public class Car {
-    private int teamNumber;
-    private double topSpeed;
-    private int acceleration;
-    private boolean nitroUsed;
-    private double handlingFactor;
-    private double currentDistanceCovered;
-    private double currentSpeed;
-    private boolean reduceSpeed;
+  private int teamNumber;
+  private double topSpeed;
+  private int acceleration;
+  private boolean nitroUsed;
+  private double handlingFactor;
+  private double currentDistanceCovered;
+  private double currentSpeed;
+  private boolean reduceSpeed;
 
-    public int getTeamNumber() {
-        return teamNumber;
-    }
+  public int getTeamNumber() {
+    return teamNumber;
+  }
 
-    public void setTeamNumber(int teamNumber) {
-        this.teamNumber = teamNumber;
-    }
+  public void setTeamNumber(int teamNumber) {
+    this.teamNumber = teamNumber;
+  }
 
-    public double getTopSpeed() {
-        return topSpeed;
-    }
+  public double getTopSpeed() {
+    return topSpeed;
+  }
 
-    public void setTopSpeed(double topSpeed) {
-        this.topSpeed = topSpeed;
-    }
+  public void setTopSpeed(double topSpeed) {
+    this.topSpeed = topSpeed;
+  }
 
-    public int getAcceleration() {
-        return acceleration;
-    }
+  public int getAcceleration() {
+    return acceleration;
+  }
 
-    public void setAcceleration(int acceleration) {
-        this.acceleration = acceleration;
-    }
+  public void setAcceleration(int acceleration) {
+    this.acceleration = acceleration;
+  }
 
-    public boolean isNitroUsed() {
-        return nitroUsed;
-    }
+  public boolean isNitroUsed() {
+    return nitroUsed;
+  }
 
-    public void setNitroUsed(boolean nitroUsed) {
-        this.nitroUsed = nitroUsed;
-    }
+  public void setNitroUsed(boolean nitroUsed) {
+    this.nitroUsed = nitroUsed;
+  }
 
-    public double getHandlingFactor() {
-        return handlingFactor;
-    }
+  public double getHandlingFactor() {
+    return handlingFactor;
+  }
 
-    public void setHandlingFactor(double handlingFactor) {
-        this.handlingFactor = handlingFactor;
-    }
+  public void setHandlingFactor(double handlingFactor) {
+    this.handlingFactor = handlingFactor;
+  }
 
-    public double getCurrentDistanceCovered() {
-        return currentDistanceCovered;
-    }
+  public double getCurrentDistanceCovered() {
+    return currentDistanceCovered;
+  }
 
-    public void setCurrentDistanceCovered(double currentDistanceCovered) {
-        this.currentDistanceCovered = currentDistanceCovered;
-    }
+  public void setCurrentDistanceCovered(double currentDistanceCovered) {
+    this.currentDistanceCovered = currentDistanceCovered;
+  }
 
-    public double getCurrentSpeed() {
-        return currentSpeed;
-    }
+  public double getCurrentSpeed() {
+    return currentSpeed;
+  }
 
-    public void setCurrentSpeed(double currentSpeed) {
-        this.currentSpeed = currentSpeed;
-    }
+  public void setCurrentSpeed(double currentSpeed) {
+    this.currentSpeed = currentSpeed;
+  }
 
-    public boolean isReduceSpeed() {
-        return reduceSpeed;
-    }
+  public boolean isReduceSpeed() {
+    return reduceSpeed;
+  }
 
-    public void setReduceSpeed(boolean reduceSpeed) {
-        this.reduceSpeed = reduceSpeed;
-    }
+  public void setReduceSpeed(boolean reduceSpeed) {
+    this.reduceSpeed = reduceSpeed;
+  }
 
-    public Car(int totalCars, int i) {
-        teamNumber = i;
-        topSpeed = 5 * (Constants.BASE_SPEED + (Constants.THRESHOLD_SPEED * i)) / 18;
-        acceleration = Constants.BASE_ACCELERATION - Constants.THRESHOLD_ACCELERATION * i;
-        nitroUsed = false;
-        handlingFactor = Constants.HANDLING_FACTOR;
-        currentDistanceCovered = (totalCars - i) * Constants.STARTING_DISTANCE;
-        currentSpeed = 0;
-        reduceSpeed = false;
-    }
+  public Car(int totalCars, int i) {
+    teamNumber = i;
+    topSpeed = 5 * (Constants.BASE_SPEED + (Constants.THRESHOLD_SPEED * i)) / 18; //Top speed: (150 + 10 * i) km per hour, and we are converting to m/s
+    acceleration = Constants.BASE_ACCELERATION - Constants.THRESHOLD_ACCELERATION * i; //Acceleration: (2 * i) meter per second square.
+
+    nitroUsed = false;
+    handlingFactor = Constants.HANDLING_FACTOR;
+    currentDistanceCovered = (totalCars - i) * Constants.STARTING_DISTANCE;
+    currentSpeed = 0;
+    reduceSpeed = false;
+  }
 }
